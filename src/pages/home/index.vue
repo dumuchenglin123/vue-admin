d8765b379a70b814879a067ddb8ab1253bc196d7
dumuchenglin123 <243494839@qq.com>
2018-03-12 20:33:44 +0800

    填充layout

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,5 +1,12 @@
 <template>
-  <p>home</p>
+  <el-container>
+  <el-aside width="200px">Aside</el-aside>
+  <el-container>
+    <el-header>Header</el-header>
+    <el-main>Main</el-main>
+    <el-footer>Footer</el-footer>
+  </el-container>
+</el-container>
 </template>
 
 <script>
@@ -12,3 +19,30 @@
     }
   }
 </script>
+
+<style>
+  .el-header, .el-footer {
+    background-color: #B3C0D1;
+    color: #333;
+    text-align: center;
+    line-height: 60px;
+  }
+
+  .el-aside {
+    background-color: #D3DCE6;
+    color: #333;
+    text-align: center;
+    line-height: 200px;
+  }
+
+  .el-main {
+    background-color: #E9EEF3;
+    color: #333;
+    text-align: center;
+    line-height: 160px;
+  }
+
+  body > .el-container {
+    margin-bottom: 40px;
+  }
+</style>
